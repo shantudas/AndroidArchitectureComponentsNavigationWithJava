0c4b37cbf0295304bbe7fd0153027cdebf9096d0
Shantu Das <shantudasbd@yahoo.com>
2019-01-21 18:40:51 +0600

    city list added

---
+++ app/src/main/java/com/snipex/shantu/androidarchitecturecomponentsnavigation/model/City.java
@@ -0,0 +1,12 @@
+package com.snipex.shantu.androidarchitecturecomponentsnavigation.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class City {
+
+
+    private String id;
+    private String name;
+    private String country;
+}
